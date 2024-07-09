0a0ae8e14ace38846a1bb42ba4d02a58fdb98bbc
Hosea Johannes Nikolaus Dietenhofer <hosea.dietenhofer@student.uibk.ac.at>
2024-07-09 16:35:48 +0200

    nvim: delete unused tokyonight colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,3 @@
-local M = {
-	"folke/tokyonight.nvim",
-	priority = 1000, -- make sure to load this before all the other start plugins
-	init = function()
-		-- Load the colorscheme here.
-		-- Like many other themes, this one has different styles, and you could load
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		vim.cmd.colorscheme("tokyonight-night")
-
-		-- remove bg color
-		function ColorMyPencils(color)
-			color = color or "tokyonight-night"
-			vim.cmd.colorscheme(color)
-
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-		end
-
-		ColorMyPencils()
-
-		-- You can configure highlights by doing something like
-		vim.cmd.hi("Comment gui=none")
-	end,
-}
-
 local C = {
   {
     "catppuccin/nvim",
