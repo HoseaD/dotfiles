3a30d8ca9da01bb6eb39184fec79e2ef989dc829
Hosea Johannes Nikolaus Dietenhofer <hosea.dietenhofer@student.uibk.ac.at>
2024-07-09 16:34:25 +0200

    nvim: add rust and toml

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ local M = {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+		ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "rust", "toml" },
 		-- Autoinstall languages that are not installed
 		auto_install = true,
 		highlight = { enable = true },
