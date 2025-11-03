be606503021d639f1cd36d6b16216ee2c7222bff
Hosea Dietenhofer <hosea.dietenhofer@student.uibk.ac.at>
2025-11-03 21:21:28 +0100

    shell: seperate mac & omarchy
    add mac & omarchy folder
    add omarchy .bashrc
    edit simlink.sh for mac and omarchy
    refactor

---
+++ simlink_omarchy.sh
@@ -0,0 +1,12 @@
+# Shell setup
+ln -s ~/dotfiles/shell/omarchy/zshrc ~/.zshrc
+ln -s ~/dotfiles/shell/omarchy/bashrc ~/.bashrc
+
+# Terminal Emulator
+ln -s ~/dotfiles/ghostty ~/.config/ghostty
+
+# Editor
+# ln -s ~/dotfiles/nvim ~/.config/nvim
+
+# Cli tools
+ln -s ~/dotfiles/yazi ~/.config/yazi
